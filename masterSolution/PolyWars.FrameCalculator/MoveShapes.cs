62b49ffca5a0ec235cb0f7752b9e82b2bf8dfd88
Kani <kani@kdev.dk>
2019-11-19 17:55:45 +0100

    FrameCalculator Move Method impl
    Created method for generic Shape move

---
+++ masterSolution/PolyWars.FrameCalculator/MoveShapes.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Media;
+using System.Windows.Shapes;
+using PolyWars.API;
+
+namespace PolyWars.FrameCalculator {
+    class MoveShapes {
+
+        void Move( IShape shape ) {
+            RotateTransform rt = new RotateTransform {
+                CenterX = shape.CenterPoint.X,
+                CenterY = shape.CenterPoint.Y,
+                Angle = shape.Angle
+            };
+            Polygon p = shape.getShapeAsPolygon();
+            p.RenderTransform = rt;
+        }
+    }
+}
