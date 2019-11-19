f00fd81ff284adf968e6659ed4f8b811a193a234
Kani <kani@kdev.dk>
2019-11-19 14:16:56 +0100

    Implemented Shell Arena and ArenaController

---
+++ masterSolution/PolyWars/Logic/ArenaController.cs
@@ -0,0 +1,23 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyWars.Logic {
+    class ArenaController {
+
+
+        /// <summary>
+        /// Used to Initilize Arena with Resources to 
+        /// determinate Arena layout
+        /// </summary>
+        public Arena initArena() {
+            throw new NotImplementedException();
+        }
+
+        private Arena generateArena() { 
+            
+        }
+    }
+}
