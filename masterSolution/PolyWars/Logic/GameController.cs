eeca656f5b79109bb471e7d3e4f376c60bc296e5
Emborg <irene.7005@hotmail.com>
2019-11-19 15:29:33 +0100

    Game ctrl implemented input methods.

--- masterSolution/PolyWars/Logic/GameController.cs
+++ masterSolution/PolyWars/Logic/GameController.cs
@@ -4,11 +4,30 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 
-namespace PolyWars.Logic
-{
-    class GameController
-    {
-        public static void PlayGame(Action msgTest) {
+namespace PolyWars.Logic {
+    class GameController {
+        private InputController inputController;
+        private bool isLoaded;
+
+        GameController() {
+            inputController = new InputController();
+            isLoaded = false;
+        }
+
+        public bool isPrepared() {
+            return isLoaded;
+        }
+
+        public void prepareGame() {
+            // TODO  load resources
+            // TODO  Init arena
+            // TODO  Init sound
+            inputController.initInput();
+            isLoaded = true;
+        }
+
+        public void playGame(Action msgTest)
+        {
             msgTest.Invoke();
         }
     }
