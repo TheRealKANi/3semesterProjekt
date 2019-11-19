ac407dec3d6c57d404e688e92d420ee779015b30
Emborg <irene.7005@hotmail.com>
2019-11-19 14:56:21 +0100

    Implemented Basic Skeleton for Launching game

--- masterSolution/PolyWars/UI/GameArena/GameArena_ViewModel.cs
+++ masterSolution/PolyWars/UI/GameArena/GameArena_ViewModel.cs
@@ -1,4 +1,5 @@
-﻿using System;
+﻿using PolyWars.Logic;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,5 +7,12 @@ using System.Threading.Tasks;
 
 namespace PolyWars.ViewModel {
     class GameArena_ViewModel {
+        private GameController gameController;
+
+        public GameArena_ViewModel() {
+            gameController = new GameController();
+            gameController.prepareGame();
+            gameController.playGame();
+        }
     }
 }
