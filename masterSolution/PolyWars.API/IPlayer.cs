06db036148bb5390370b194d6e6388814b5f4ebc
Kani <kani@kdev.dk>
2019-11-22 10:58:15 +0100

    IPlayer - Created

---
+++ masterSolution/PolyWars.API/IPlayer.cs
@@ -0,0 +1,17 @@
+﻿using PolyWars.API;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PolyWars.Model {
+
+    /// <summary>
+    /// Creates a basic player with an Shape
+    /// </summary>
+    interface IPlayer : IShape {
+
+
+    }
+}
