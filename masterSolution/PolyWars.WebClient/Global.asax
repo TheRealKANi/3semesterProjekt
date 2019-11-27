a7e4ccce39b148d711e5b98ee0a927eedf93b641
Levente Huszti <huszti@live.dk>
2019-11-27 13:45:02 +0100

    Created empty Web client project
    ASP.NET, MVC, Individual Authentication

---
+++ masterSolution/PolyWars.WebClient/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PolyWars.WebClient.MvcApplication" Language="C#" %>
---
+++ masterSolution/PolyWars.WebClient/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PolyWars.WebClient.Startup))]
+namespace PolyWars.WebClient
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
