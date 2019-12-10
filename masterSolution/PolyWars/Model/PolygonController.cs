5cbf389cd96cfbffe7b71cf4faabd258f469249d
Thure Christensen <thurec@protonmail.com>
2019-12-11 00:33:04 +0100

    such forgetful

---
+++ masterSolution/PolyWars/Model/PolygonController.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Shapes;
+
+namespace PolyWars.Model {
+    class PolygonController {
+        private static PolygonController polygonController;
+        private List<Polygon> polygons;
+
+        static PolygonController() {
+            polygonController = new PolygonController();
+            polygonController.polygons = new List<Polygon>();
+        }
+
+        //static Polygon this[int i] {
+        //    get {
+        //        return polygons[i];
+        //    }
+        //}
+    }
+}
