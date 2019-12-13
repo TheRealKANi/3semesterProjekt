6dd4148b9eac9924ba9fd5fe33bb20702ffb571f
Levente Huszti <huszti@live.dk>
2019-12-13 04:27:02 +0100

    Added register and login form

---
+++ masterSolution/PolyWars.Web.Client/Controllers/AccountController.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace PolyWars.Web.Client.Controllers {
+    public class AccountController : Controller {
+
+        //URL: /Account/Register
+        public ActionResult Register() {
+            return View();
+        }
+
+        //URL: /Account/Login
+        public ActionResult Login() {
+            return View();
+        }
+    }
+}
