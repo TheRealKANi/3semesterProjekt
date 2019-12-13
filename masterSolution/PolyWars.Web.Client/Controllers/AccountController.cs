3e5761e839f856f7450085b05a989884cff470c9
Levente Huszti <huszti@live.dk>
2019-12-13 21:29:32 +0100

    Added logic to login and register thru server service

--- masterSolution/PolyWars.Web.Client/Controllers/AccountController.cs
+++ masterSolution/PolyWars.Web.Client/Controllers/AccountController.cs
@@ -1,7 +1,6 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Web;
+﻿using PolyWars.Web.Client.Models;
+using PolyWars.Web.Client.ServiceReferenceWCF;
+using System.Threading.Tasks;
 using System.Web.Mvc;
 
 namespace PolyWars.Web.Client.Controllers {
@@ -12,9 +11,60 @@ namespace PolyWars.Web.Client.Controllers {
             return View();
         }
 
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Register(RegisterViewModel model) {
+            if(ModelState.IsValid) {
+                WebClientServiceClient client = new WebClientServiceClient();
+                UserData userData = new UserData() {
+                    userName = model.UserName,
+                    password = model.Password
+                };
+                if(client.register(userData)) {
+                    if(client.login(userData)) {
+                        return RedirectToAction("Index", "Home");
+                    }
+                }
+            }
+            // If we got this far, something failed, redisplay form
+            return View(model);
+        }
+
+
         //URL: /Account/Login
         public ActionResult Login() {
             return View();
+        }
+
+        // POST: /Account/Login
+        [HttpPost]
+        [AllowAnonymous]
+        [ValidateAntiForgeryToken]
+        public async Task<ActionResult> Login(LoginViewModel model, string returnUrl) {
+            if(!ModelState.IsValid) {
+                return View(model);
+            }
+            WebClientServiceClient client = new WebClientServiceClient();
+            UserData userData = new UserData() {
+                userName = model.UserName,
+                password = model.Password
+            };
+
+            if(client.login(userData)) {
+
+                return RedirectToLocal(returnUrl);
+            } else {
+
+                return View(model);
+            }
+        }
+
+        private ActionResult RedirectToLocal(string returnUrl) {
+            if(Url.IsLocalUrl(returnUrl)) {
+                return Redirect(returnUrl);
+            }
+            return RedirectToAction("Index", "Home");
         }
     }
 }
