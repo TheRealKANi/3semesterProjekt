5b6fab4fb210994595d25ad763eb44527546a684
kani <kani@kdev.dk>
2019-12-15 11:23:43 +0100

    PolyWars.Client - Clean Up and code docs - Done

--- masterSolution/PolyWars.Client/Adapters/BulletAdapter.cs
+++ masterSolution/PolyWars.Client/Adapters/BulletAdapter.cs
@@ -10,10 +10,20 @@ using System.Threading.Tasks;
 using System.Windows.Media;
 
 namespace PolyWars.Adapters {
+    /// <summary>
+    /// Base class for converting bullets to and from the server format
+    /// and adding to and from the arena
+    /// </summary>
     class BulletAdapter {
+        private static int bulletWidth = 4;
+        private static int bulletHight = 4;
+        private static int bulletVertices = 40; // circle-ish
+        private static int bulletLineThickness = 1;
+        private static int bulletLineMaxVelocity = 19;
+        private static Color bulletColor = Colors.DarkViolet;
+        private static Color bulletBorderColor = Colors.Black;
         public static async Task<ConcurrentDictionary<string, IBullet>> BulletDTOAdapter() {
             List<BulletDTO> bulletDTOs = await NetworkController.GameService.getBulletsAsync();
-            //Console.WriteLine("Client - Got Resources from server");
             return BulletDTOtoIBullet(bulletDTOs);
         }
 
@@ -30,9 +40,10 @@ namespace PolyWars.Adapters {
         }
 
         public static Bullet renderBullet(BulletDTO bullet) {
-            IRenderable renderable = new Renderable(Brushes.Black.Color, Brushes.DarkViolet.Color, 1, 4, 4, 40);
+            IRenderable renderable = new Renderable(bulletBorderColor, bulletColor, bulletLineThickness,
+                bulletWidth, bulletHight, bulletVertices);
             IShape shape = new Shape(bullet.ID, bullet.Ray, renderable, new RenderStrategy());
-            IMoveable bulletShip = new Moveable(18, 19, 0, 0, shape, new MoveStrategy());
+            IMoveable bulletShip = new Moveable(bulletLineMaxVelocity - 1, bulletLineMaxVelocity, 0, 0, shape, new MoveStrategy());
             return new Bullet(bullet.ID, bulletShip, bullet.Damage, bullet.PlayerID);
         }
 
@@ -40,19 +51,19 @@ namespace PolyWars.Adapters {
             if(GameController.Bullets != null && GameController.Bullets.ContainsKey(bulletID)) {
                 UIDispatcher.Invoke(() => {
                     IBullet b;
-                    while(!GameController.Bullets.TryRemove(bulletID, out b)) {
-                        Task.Delay(1);
-                    }
+                    while(!GameController.Bullets.TryRemove(bulletID, out b)) { Task.Delay(1); }
                     ArenaController.ArenaCanvas.Children.Remove(b.BulletShip.Shape.Polygon);
                 });
             }
         }
 
         public static void addBulletToCanvas(Bullet bullet) {
-            bullet.BulletShip.Shape.Renderer.Render(bullet.BulletShip.Shape.Renderable, bullet.BulletShip.Shape.Ray);
-            UIDispatcher.Invoke(() => {
-                ArenaController.ArenaCanvas.Children.Add(bullet.BulletShip.Shape.Polygon);
-            });
+            if(GameController.Bullets != null && !GameController.Bullets.ContainsKey(bullet.ID)) {
+                bullet.BulletShip.Shape.Renderer.Render(bullet.BulletShip.Shape.Renderable, bullet.BulletShip.Shape.Ray);
+                UIDispatcher.Invoke(() => {
+                    ArenaController.ArenaCanvas.Children.Add(bullet.BulletShip.Shape.Polygon);
+                });
+            }
         }
 
         public static BulletDTO bulletToDTO(IBullet bullet) {
--- masterSolution/PolyWars.Client/Adapters/ResourceAdapter.cs
+++ masterSolution/PolyWars.Client/Adapters/ResourceAdapter.cs
@@ -1,28 +1,32 @@
 ﻿using PolyWars.Api.Model;
 using PolyWars.API.Model.Interfaces;
 using PolyWars.API.Network.DTO;
-using PolyWars.API.Strategies;
-using PolyWars.Logic;
-using PolyWars.Network;
 using PolyWars.Server.Model;
 using PolyWars.ServerClasses;
-using System;
-using System.Collections.Concurrent;
-using System.Collections.Generic;
-using System.Diagnostics;
-using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Media;
 
 namespace PolyWars.Adapters {
+    /// <summary>
+    /// Base class for converting resources to and from the server format
+    /// </summary>
     public class ResourceAdapter {
+        private static int resourceWidth = 15;
+        private static int resourceHight = 15;
+        private static int resourceVertices = 4; // Square
+        private static int resourceLineThickness = 1;
+        private static Color resourceColor = Colors.ForestGreen;
+        private static Color resourceBorderColor = Colors.Black;
+
         public static IResource DTOToResource(ResourceDTO dto) {
             IRay ray = new Ray(dto.ID, new Point(dto.CenterX, dto.CenterY), dto.Angle);
-            IRenderable renderable = new Renderable(Colors.Black, Colors.ForestGreen, 1, 15, 15, 4);
+            IRenderable renderable = new Renderable(resourceBorderColor, resourceColor,
+                resourceLineThickness, resourceWidth, resourceHight, resourceVertices);
+
             IShape shape = new Shape(dto.ID, ray, renderable, new RenderStrategy());
-            
             return new Resource(dto.ID, shape, dto.Value);
         }
+
         public static ResourceDTO IResourceToDTO(IResource resource) {
             return new ResourceDTO {
                 ID = resource.ID,
